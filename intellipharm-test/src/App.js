b651b9396251cae3ed684c23bcee5d423fbc147a
TraceWright <traceywright27@gmail.com>
2018-06-23 21:45:05 +1000

    add graph; use throttle method for updating graph data; styling

--- intellipharm-test/src/App.js
+++ intellipharm-test/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import { HorizontalBar } from 'react-chartjs-2';
+import { debounce } from 'throttle-debounce';
+import randomColor from 'randomcolor';
 
 class App extends Component {
   constructor(props) {
     super(props);
+      this.resetGraphTableData = debounce(500, this.resetGraphTableData);
       this.state = {
         apiData: [],
         filteredData: null,
         currentPage: 1,
         itemsPerPage: 100,
         inputText: '',
+        graphData: null,
       }
+
     }
 
   filterApiData = (data, targetValue) => {
@@ -28,12 +34,22 @@ class App extends Component {
         ? element.firstname : null
     });
 
+    console.log(dataFiltered)
+
     this.setState({ filteredData: dataFiltered });
+    this.setState({ graphData: this.signupsPerYear(dataFiltered) })
+  }
+
+  triggerSearch(e) {
+    this.resetGraphTableData(e.target.value);
+  }
+  resetGraphTableData(value) {
+    console.log(value)
+    this.filterApiData(this.state.apiData, value);
   }
 
   handleTextChange = (event) => {
     this.setState({ inputText: event.target.value });
-    this.filterApiData(this.state.apiData, event.target.value);
   }
 
   handleClick = (event) => {
@@ -42,7 +58,7 @@ class App extends Component {
     });
   }
 
-  generatePageNumbers = (data, itemsPerPage) => {
+  generatePageNumbers(data, itemsPerPage) {
     const pageNumbers = [];
     for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
       pageNumbers.push(i);
@@ -53,15 +69,27 @@ class App extends Component {
   componentWillMount = () => {
     axios.get('https://intellipharm.com.au/devtest/index.php')
       .then((response) => (
-        this.setState({ apiData: response.data })
+        this.setState({ apiData: response.data }),
+        this.setState({ graphData: this.signupsPerYear(response.data) })
       ))
       .catch((error) => (
         console.log(error)
-      ));
+      ));  
   }
 
+  signupsPerYear = (data) => {
+    let signPerYr = {};
+    data.map(element => (
+      !!signPerYr[new Date(Date.parse(element.joined_date)).getFullYear()] === true
+      ? signPerYr[new Date(Date.parse(element.joined_date)).getFullYear()] = signPerYr[new Date(Date.parse(element.joined_date)).getFullYear()] + 1
+      : signPerYr[new Date(Date.parse(element.joined_date)).getFullYear()] = 1
+    ));
+    return signPerYr;
+  }
 
   render() {
+
+
 
     const { apiData, currentPage, itemsPerPage, filteredData } = this.state;
     const indexOfLastItem = currentPage * itemsPerPage;
@@ -80,16 +108,6 @@ class App extends Component {
 
     const pageNumbers = this.generatePageNumbers(filteredData === null ? apiData : filteredData, itemsPerPage);
 
-    // if (filteredData === null) {
-    //   for (let i = 1; i <= Math.ceil(apiData.length / itemsPerPage); i++) {
-    //     pageNumbers.push(i);
-    //   }
-    // } else {
-    //    for (let i = 1; i <= Math.ceil(filteredData.length / itemsPerPage); i++) {
-    //     pageNumbers.push(i);
-    //   }
-    // }
-
     const renderPageNumbers = pageNumbers.map(number => {
       return (
         <li
@@ -105,18 +123,53 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div>
-          <input
-            onChange={this.handleTextChange}
-            value={this.state.inputText}
-          ></input>
-
+        <div className="search-area">
+          <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <label>Search:</label>
+                  </td>
+                  <td>
+                    <input
+                      id="text-input"
+                      onChange={this.handleTextChange}
+                      value={this.state.inputText}
+                      type="text"
+                      onKeyUp={this.triggerSearch.bind(this)}
+                    ></input>
+                  </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-        <div className="data-table">
-          <table>
+        <div className="graph-area">
+          <HorizontalBar 
+            data={{
+              labels: this.state.graphData !== null ? Object.keys(this.state.graphData) : [],
+              datasets: [
+              {
+                label: 'Signup Year (count)',
+                backgroundColor: randomColor(),
+                borderColor: randomColor(),
+                borderWidth: 1,
+                hoverBackgroundColor: randomColor(),
+                hoverBorderColor: randomColor(),
+                data: this.state.graphData !== null ? Object.values(this.state.graphData) : []
+              }
+            ]}}
+           height={500}
+           options={{
+            maintainAspectRatio: false
+        }}
+          />
+        </div>
+
+        <div className="table-area">
+          <table className="data-table">
             <tbody>
-              <tr>
+              <tr className="headings">
                 <td>Firstname</td>
                 <td>Lastname</td> 
                 <td>Email</td>
@@ -130,9 +183,9 @@ class App extends Component {
           </table>
         </div>
 
-        <ul id="page-numbers">
-          {renderPageNumbers}
-        </ul>
+          <ul id="page-numbers">
+            {renderPageNumbers}
+          </ul>
 
       </div>
     );
